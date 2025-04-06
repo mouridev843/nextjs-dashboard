009fde5501504c79f83a4f6f39a078f497eeaff8
mouridev843 <ahmeddameseck7@gmail.com>
2025-04-06 18:14:57 +0000

    Prêt pour GitHub

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-24">
+      <h1 className="text-3xl font-bold">Invoices Page</h1>
+      <p className="mt-4 text-lg">Manage your invoices here.</p>
+    </div>
+  );
+}
